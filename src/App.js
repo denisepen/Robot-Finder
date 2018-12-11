7b16578e8d825c66ba04213c6a4afa289aa69485
Denise Pendleton-Lipinski <denisependletonlipinski@Denises-MacBook-Pro.local>
2018-12-11 08:05:29 -0500

    added component did mount and pull from json user jsonplaceholders list

--- src/App.js
+++ src/App.js
@@ -14,9 +14,18 @@ import SearchBox from './SearchBox'
       }
     }
 
+    componentDidMount(){
+      fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => this.setState({
+        robots: users
+      }))
+    }
+
     onSearchChange = (e) => {
       // e.preventDefault();
       this.setState({ searchfield: e.target.value})
+      // console.log(this.state)
     }
 
 
@@ -31,7 +40,7 @@ import SearchBox from './SearchBox'
     return (
 
       <div className = 'tc' >
-        <h1> Robo Friends </h1>
+        <h1> Robot Finder </h1>
         <SearchBox  searchChange = {this.onSearchChange}/>
         <CardList robots={filteredRobots}/>
       </div>
